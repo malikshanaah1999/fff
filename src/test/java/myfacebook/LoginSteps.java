d51ce6679ea84726e69d17440f0ddaa8dd775597
Malik Shanaah <s11717878@stu.najah.edu>
2023-01-12 18:02:21 +0200

    fffF

--- src/test/java/myfacebook/LoginSteps.java
+++ src/test/java/myfacebook/LoginSteps.java
@@ -1,5 +1,43 @@
 package myfacebook;
 
-public class LoginSteps {
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
+public class LoginSteps {
+	@Given("The Administrator is on the login page.")
+	public void the_administrator_is_on_the_login_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The Admin. enters a valid username and password.")
+	public void the_admin_enters_a_valid_username_and_password() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("Clicks on the login button.")
+	public void clicks_on_the_login_button() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The user should be navigated to the Home page.")
+	public void the_user_should_be_navigated_to_the_home_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The Admin. entered a wrong username or password.")
+	public void the_admin_entered_a_wrong_username_or_password() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The Administrator will still on the login page.")
+	public void the_administrator_will_still_on_the_login_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
 }
